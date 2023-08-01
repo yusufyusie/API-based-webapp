c4a0ec809635e9e3d440f15970898764f9a24952
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-08-01 19:26:50 +0100

    Retrieve Meal details

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import MealList from './modules/meallist.js';
 import './style.css';
 
-const form = document.querySelector('form');
-const btnRefresh = document.querySelector('#btnRefresh');
 const mealNew = new MealList();
 
 document.body.onload = () => {
-  if (mealNew.gameId === null) mealNew.createGame();
+  mealNew.getMeal(null, 52772);
+  // if (mealNew.gameId === null) mealNew.createApp();
 };
-
-//  add an event listener to the refresh button
-btnRefresh.addEventListener('click', () => {
-  mealNew.getGames(btnRefresh);
-});
-
-//  add an event listener to the form submit button
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const name = document.querySelector('#name').value.trim();
-  const score = document.querySelector('#score').value;
-  if (name !== '') {
-    const meal = {
-      name,
-      score,
-    };
-    mealNew.postScore(meal);
-    document.querySelector('#name').value = '';
-    document.querySelector('#score').value = '';
-  }
-});
