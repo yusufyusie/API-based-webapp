0bc51f2432166d6406dc90ab2a3dc749006eb3a5
yusufyusie <yesuffenta023@gmail.com>
2023-08-02 09:42:15 +0300

    Update card files to support home display

--- src/modules/cards/mealCard.js
+++ src/modules/cards/mealCard.js
@@ -7,18 +7,11 @@ const ul = document.createElement('ul');
 ul.className = 'mealItem';
 const strUl = document.createElement('ul');
 strUl.className = 'strUl';
-const buttonUl = document.createElement('ul');
-buttonUl.className = 'buttonUl';
-
-const buttonComment = document.createElement('ul');
-buttonComment.className = 'button';
 
 ul.innerHTML = `<img class='thumb' src='${imageId}'>`;
 strUl.innerHTML = `<span>${title}</span>`;
 
 ul.appendChild(strUl);
-buttonUl.appendChild(buttonComment);
-ul.appendChild(buttonUl);
 board.appendChild(ul);
 };
 
