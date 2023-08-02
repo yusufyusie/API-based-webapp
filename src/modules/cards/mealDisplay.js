8529dc1a1fed6e49ebf1746ba32482105920a976
yusufyusie <yesuffenta023@gmail.com>
2023-08-02 20:44:39 +0300

    fix linter error for adding likes on eslinter

--- src/modules/cards/mealDisplay.js
+++ src/modules/cards/mealDisplay.js
@@ -24,7 +24,7 @@ const mealDisplay = async () => {
                 meal.idMeal,
                 meal.strMealThumb,
                 likeNum,
-                meal.strTags
+                meal.strTags,
              );
     });
   };
