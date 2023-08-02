b6d68c2166c8ede583eecbb141275fad005411bc
yusufyusie <yesuffenta023@gmail.com>
2023-08-02 14:36:35 +0300

    Add ui for display likes

--- src/modules/cards/mealCard.js
+++ src/modules/cards/mealCard.js
@@ -1,17 +1,32 @@
+import like from '../likes/heart.svg';
 import displayMeals from './getMeals.js';
 
-const mealCard = (title, id, imageId) => {
+const myLike = new Image();
+myLike.src = like;
+myLike.classList = 'imgLike';
+
+const mealCard = (title, id, imageId, likeNum) => {
 const board = document.querySelector('.meals-card');
 
 const ul = document.createElement('ul');
 ul.className = 'mealItem';
+
 const strUl = document.createElement('ul');
 strUl.className = 'strUl';
 
+const likeUl = document.createElement('ul');
+likeUl.className = 'likeDiv';
+
+const buttonUl = document.createElement('ul');
+buttonUl.className = 'buttonDiv';
+
 ul.innerHTML = `<img class='thumb' src='${imageId}'>`;
 strUl.innerHTML = `<span>${title}</span>`;
+likeUl.innerHTML = `<span class='hide'>${id}</span><img class='imgLike' src='${like}'><ul class="stop">${likeNum}</ul><p class="stop">Likes</p>`;
 
+strUl.appendChild(likeUl);
 ul.appendChild(strUl);
+ul.appendChild(buttonUl);
 board.appendChild(ul);
 };
 
---
+++ src/modules/likes/postLike.js
@@ -0,0 +1,20 @@
+const URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/hMYaxRWDnnbjDMdtxmy3/likes/';
+
+const postLike = async (idMeal) => {
+    const response = await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: idMeal,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    
+    .then(async (response) => {
+    const data = await response.json();
+    return data;
+    });
+  };
+
+  export default postLike;
